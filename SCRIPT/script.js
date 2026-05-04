f6e308fc95ce60ec2af0017ac787b471aff76d7a
arthurrobertoalvescastrobarro-png <arthur.roberto.alves.castro.barro@gmail.com>
2026-05-04 11:04:44 -0300

    AR

---
+++ SCRIPT/script.js
@@ -0,0 +1,9 @@
+function salvarCarros(event) {
+    event.preventDefault();
+
+    let preco = document.getElementById('preco').value;
+
+    let marca = document.getElementById('marca').value;
+    
+    let modelo = document.getElementById('modelo').value;
+}
